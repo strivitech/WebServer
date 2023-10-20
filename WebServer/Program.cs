33a8c167784d40a4b2b7f24622421d010c7b72e6
strivitech <strivitech@gmail.com>
2023-10-20 19:23:09 +0300

    Add Program.cs

---
+++ WebServer/Program.cs
@@ -0,0 +1,6 @@
+﻿using WebServer.Core;
+
+var controllerApiHandler = new ControllerApiHandler(new RequestReader(), new ResponseWriter(),
+    new ControllerActionInvoker(new ControllerFactory()));
+var server = new Server(controllerApiHandler);
+await server.RunAsync();
