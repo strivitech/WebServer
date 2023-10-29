d80663e95f4129e1723ad5264b9cc7d05a4947a4
strivitech <strivitech@gmail.com>
2023-10-30 01:04:49 +0200

    Add web request path parsing util classes

---
+++ WebServer.Core/WebRequestRoute.cs
@@ -0,0 +1,21 @@
+﻿namespace WebServer.Core;
+
+public class WebRequestRoute
+{
+    public string ControllerRoute { get; }
+
+    public string ActionName { get; }
+
+    public List<string> UrlParameters { get; }
+
+    public Dictionary<string, string?> QueryParameters { get; }
+
+    public WebRequestRoute(string controllerRoute, string actionName, List<string> urlParameters,
+        Dictionary<string, string?> queryParameters)
+    {
+        ControllerRoute = controllerRoute;
+        ActionName = actionName;
+        UrlParameters = urlParameters;
+        QueryParameters = queryParameters;
+    }
+}
