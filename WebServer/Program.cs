85128d9c469dc3ffd1de5a7e790058805e3159d5
strivitech <strivitech@gmail.com>
2023-11-11 20:58:56 +0200

    Update Programs.cs with new application builder and app classes

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -1,43 +1,27 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 using WebServer;
-using WebServer.Core;
 using WebServer.Core.Application;
 using WebServer.Core.Configuration;
-using WebServer.Core.ControllersContext;
 using WebServer.Core.ControllersContext.Actions;
 using WebServer.Core.MinimalApiContext;
-using WebServer.Core.ModelBinders;
-using WebServer.Core.Request;
-using WebServer.Core.Request.Headers;
-using WebServer.Core.Response;
-using WebServer.Core.Response.Builder;
-using WebServer.Core.Transport;
 
-var store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
-store.Open(OpenFlags.ReadOnly);
-var certificate = store.Certificates.Find(X509FindType.FindBySubjectName, "localhost", false)[0];
-store.Close();
-
-var serverConfiguration = new ServerConfiguration
-{
-    TlsSettings = new TlsSettings
+var appBuilder = new AppBuilder()
+    .Configure(() => new ServerConfiguration
     {
-        UseTls = true,
-        Certificate = new TlsCertificate
+        TlsSettings = new TlsSettings
         {
-            X509Certificate2 = certificate
+            UseTls = true,
+            Certificate = new TlsCertificate
+            {
+                X509Certificate2 = GetCertificate()
+            }
         }
-    }
-};
+    })
+    .AddRequestProcessor(RequestProcessorType.MinimalApi);
 
-// var requestHandler = new HttpRequestHandler(new HttpRequestReader(new HttpRequestHeadersValidator(
-//     new ContentTypeValidator(),
-//     new ContentLengthValidator())),
-//     new HttpResponseWriter(new ResponseBuilder()),
-//     new ControllerProcessor(new ControllerFactory(), new BindersFactory(), new ActionInfoFetcherFactory()));
+var app = appBuilder.Build();
 
-IAppBuilder appBuilder = new AppBuilder();
-appBuilder.UseEndpoints()
+app.UseEndpoints()
     .MapGet("/api/MyController", () => Results.Ok("Hello World!"))
     .MapPost("/api/MyController", async ([FromBody] Person person,
         [FromParameters] string country,
@@ -55,11 +39,13 @@ appBuilder.UseEndpoints()
         Car = car
     }));
 
-var requestHandler = new HttpRequestHandler(new HttpRequestReader(new HttpRequestHeadersValidator(
-        new ContentTypeValidator(),
-        new ContentLengthValidator())),
-    new HttpResponseWriter(new ResponseBuilder()),
-    new MinimalApiProcessor(new BindersFactory()));
+await app.RunAsync();
 
-var server = new TcpBasedServer(requestHandler, serverConfiguration);
-await server.StartAsync();
+X509Certificate2 GetCertificate()
+{
+    var store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+    store.Open(OpenFlags.ReadOnly);
+    var x509Certificate2 = store.Certificates.Find(X509FindType.FindBySubjectName, "localhost", false)[0];
+    store.Close();
+    return x509Certificate2;
+}
